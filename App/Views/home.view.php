adeba5f27f1fb6b84f87c9e3173c53bce8e09e49
Masther21 <luisalfredo14054@gmail.com>
2026-06-30 14:33:20 -0400

    Create home.view.php
    Pantalla de Inicio

---
+++ App/Views/home.view.php
@@ -0,0 +1,11 @@
+<div class="login-container">
+  <img src="https://randomuser.me/api/portraits/men/32.jpg" alt="Avatar">
+  <h2>Login</h2>
+  <form id="frm_Login">
+    <input id="frm_Inp_mail"  name="frm_Inp_mail"   type="text" placeholder="Usuario" required>
+    <input id="frm_Inp_pass" name="frm_Inp_pass"  type="text" placeholder="Contraseña" required>
+    <!-- <input id="frm_Inp_pass" name="frm_Inp_pass"  type="password" placeholder="Contraseña" required> -->
+    <button type="submit">Ingresar</button>
+  </form>
+</div>
+
