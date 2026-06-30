8b2acd3d0ac659470978a9c4bd4ee873719ad826
Masther21 <luisalfredo14054@gmail.com>
2026-06-30 14:34:55 -0400

    Create Autoload.php
    Carga de Archivos o carpetas

---
+++ App/Autoload.php
@@ -0,0 +1,9 @@
+<?php
+    require_once(__DIR__. '/Helpers/Common.php');
+    require_once(__DIR__. '/Core/Controller.php');
+    require_once(__DIR__. '/Core/Orm.php');
+    require_once(__DIR__. '/Services/Database.php'); 
+    require_once(__DIR__. '/Config.php');
+    require_once(__DIR__. '/Router.php');
+
+    
