8ccba4de98e5aa164dafad9dc76fa76738c0baa8
Masther21 <luisalfredo14054@gmail.com>
2026-06-30 14:29:28 -0400

    Create head.php
    Encabezado de la Web

---
+++ App/Views/File_php/head.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>.:Choose-Chat(.:2C:.):.</title>
+        <link rel="stylesheet" href="<?= URL_PATH ?>/Assets/CSS/estilos.css">
+        <script> var URL_PATH = "<?= URL_PATH ?>"; </script>
+    </head>
+    <body>
