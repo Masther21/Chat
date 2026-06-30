93df7cd16c3a7fd61f38f59d65a8ae66c053a275
Masther21 <luisalfredo14054@gmail.com>
2026-06-30 14:28:35 -0400

    Create footer.php
    Pie de la pagina

---
+++ App/Views/File_php/footer.php
@@ -0,0 +1,9 @@
+
+        <!-- Script Ingresar el Usuario -->
+            <script src="<?= URL_PATH ?>/Assets/JS/UsuarioNew.js"></script>
+        <!-- Script Login -->
+            <script src="<?= URL_PATH ?>/Assets/JS/login.js"></script>
+        <!-- Script Login -->
+            <script src="<?= URL_PATH ?>/Assets/JS/loguedUser.js"></script>
+    </body>
+</html>
